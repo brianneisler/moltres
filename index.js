a5fdf336efc0d187d755b4195dc54305bb71ae0f
Brian Neisler <me@brianneisler.com>
2020-08-04 11:48:31 -0700

    Do not use NODE_ENV for determining whether to use babel-register in index file since

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-if (process.env.NODE_ENV === 'development') {
+// NOTE BRN: We use a custom env variable here instead of NODE_ENV because when
+// this binary is loaded for development by another application they may have
+// set their NODE_ENV to "development" but have installed this module for
+// production usage
+if (process.env.MOLTRES_CLI_TEST) {
   require('@babel/register')(require('./babel.config'))
   module.exports = require('./src')
 } else {
