aa2fa2dead20e5f2db86e11a0baecf1a80bac397
Brian Neisler <me@brianneisler.com>
2021-03-14 15:21:26 -0700

    Fix zip utils

--- src/zip/index.js
+++ src/zip/index.js
@@ -1,3 +1,3 @@
 export { default as unzipAllTo } from './unzipAllTo'
-export { default as unzipContentsTo } from './unzipContentsTo'
-export { default as zipDirectory } from './zipDirectory'
+export { default as unzipContentsTo } from './unzipEntryTo'
+export { default as zipDirectoryToFilePath } from './zipDirectoryToFilePath'
---
+++ src/zip/zipDirectoryToFilePath.web.js
@@ -0,0 +1,14 @@
+/**
+ * Zips up a target directory and outputs it to the given file path.
+ *
+ * @function
+ * @since v0.6.0
+ * @category zip
+ * @param {*} directory
+ * @param {*} outputFilePath
+ */
+const zipDirectoryToFilePath = async () => {
+  throw new Error('NOT IMPLEMENTED')
+}
+
+export default zipDirectoryToFilePath
